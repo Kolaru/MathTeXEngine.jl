4aba85a59e4e861cbc3dd2c476d1f917206bb536
Benoît Richard <kolaru@hotmail.com>
2022-03-28 02:11:42 +0200

    Compact show for TeXExpr

--- src/parser/texexpr.jl
+++ src/parser/texexpr.jl
@@ -116,8 +116,53 @@ function AbstractTrees.printnode(io::IO, texexpr::TeXExpr)
     end
 end
 
-function Base.show(io::IO, texexpr::TeXExpr)
-    print_tree(io, texexpr, maxdepth=10)
+to_latex(::Nothing) = nothing
+
+function to_latex(texexpr::TeXExpr)
+    head = texexpr.head
+    args = texexpr.args
+
+    head in [:char, :digit, :punctuation, :symbol] && return string(first(args))
+    head == :function && return "\\$(first(args))"
+    head == :frac && return "\\frac{$(to_latex(args[1]))}{$(to_latex(args[2]))}"
+    head == :group && return join(to_latex.(args), " ")
+    head == :sqrt && return "\\sqrt{$(to_latex(first(args)))}"
+    head == :spaced && return string(first(first(args).args))
+    head == :space && return ""
+    
+    if head in [:decorated, :integral, :underover]
+        core, sub, sup = to_latex.(args)
+
+        if !isnothing(sub)
+            if length(sub) == 1
+                core *= "_$sub"
+            else
+                core *= "_{$sub}"
+            end
+        end
+
+        if !isnothing(sup)
+            if length(sup) == 1
+                core *= "^$sup"
+            else
+                core *= "^{$sup}"
+            end
+        end
+        return core
+    end
+
+    # TODO :accent, :space properly
+
+    # Fallback
+    return "to_latex($texexpr)"
+end
+
+function Base.show(io::IO, ::MIME"text/plain", texexpr::TeXExpr)
+    if haskey(io, :compact) && io[:compact]
+        print(io, "TeX\"$(to_latex(texexpr))\"")
+    else
+        return print_tree(io, texexpr, maxdepth=10)
+    end
 end
 
 function Base.:(==)(tex1::TeXExpr, tex2::TeXExpr)
