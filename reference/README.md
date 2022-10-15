23050c4cbb16d2df62d4ad657bc2c02eb7a8ad8c
Benoît Richard <kolaru@hotmail.com>
2022-10-15 17:28:23 +0200

    Refactor a bit

---
+++ reference/README.md
@@ -0,0 +1,8 @@
+# Reference test
+
+To run the reference tests, simply run `compare.jl` twice.
+
+First on the master branch, it will create the reference images.
+
+Then on the current branch to test, it will generate the corresponding
+reference images and test they are the same.
--- reference/compare.jl
+++ reference/compare.jl
@@ -4,23 +4,29 @@ using Git
 using MathTeXEngine
 using Test
 
-include("reference/generate_reference.jl")
+include("references.jl")
 
 const git = Git.git()
 
 @testset begin "Reference images"
     master_id = readchomp(`$git rev-parse --short master`)
     current_id = readchomp(`$git rev-parse --short HEAD`)
-    @info "Comparing reference on the master $master_id with current commit $current_id"
 
     rm("reference/$current_id", recursive = true, force = true)
     generate("reference/$current_id")
 
+    if master_id == current_id
+        @info "Reference test started on master branch, nothing to compare"
+        return
+    end
+
     if !isdir("reference/$master_id")
-        @warn "No reference available for master commit $master_id"
+        @warn "No reference available for master commit $master_id, aborting"
         @test_broken false
         return
     end
+
+    @info "Comparing reference on the master $master_id with current commit $current_id"
 
     # Compare
     rm("reference/comparisons", recursive = true, force = true)
